b688858fee59fa4eb9f8c280d42b6aea5da01eaf
Krishna Glick <unstoppable411@gmail.com>
2015-09-26 01:55:08 -0400

    Bout where was before

--- config/policies.js
+++ config/policies.js
@@ -19,6 +19,10 @@
 
 module.exports.policies = {
 
+  AuthController: {
+    '*': true
+  }
+  
   /***************************************************************************
   *                                                                          *
   * Default policy for all controllers and actions (`true` allows public     *
--- config/routes.js
+++ config/routes.js
@@ -22,28 +22,11 @@
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` (or `views/homepage.jade`, *
-  * etc. depending on your default view engine) your home page.              *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+  'post /login': 'AuthController.login',
 
-  '/': {
-    view: 'homepage'
-  }
+  '/logout': 'AuthController.logout',
 
-  /***************************************************************************
-  *                                                                          *
-  * Custom routes here...                                                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the custom routes above, it   *
-  * is matched against Sails route blueprints. See `config/blueprints.js`    *
-  * for configuration options and examples.                                  *
-  *                                                                          *
-  ***************************************************************************/
+  '/api/v1/restaurants' : 'RestaurantController.find',
+  '/api/v1/restaurants/*' : 'RestaurantController.findOne'
 
 };
